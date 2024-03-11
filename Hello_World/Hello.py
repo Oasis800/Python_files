0f7aaf904575c5a2e05d6d0f4cf876102c3b4086
oasis636 <125544598+oasis636@users.noreply.github.com>
2024-03-11 17:59:20 +0000

    Easy is real

---
+++ Hello_World/Hello.py
@@ -0,0 +1,18 @@
+"""
+My first python program
+Here, I have used print function 
+to output some few lines of code
+"""
+
+name = "Kofi Mensah"
+age = 20
+department = "Accounting amd finance"
+
+#print the name of the person
+print(" My name is .", name)
+
+#print the age of the person
+print("I am ", age , "years old.")
+
+#print the department
+print("I work in the ", department, "department.")
